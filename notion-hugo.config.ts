6ff6a21eaea43e40fbbbae048aab6239d9b4e183
Jarvis Ma <realjarvisma@icloud.com>
2024-07-18 08:32:42 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/realjarvisma/Notion-DoIt-a05bb260a39f417384e3b3d6a441aae2?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
